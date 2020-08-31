e099ccb5c369bdb1a24755b456036751ef59a9aa
Camden Miller <camdenquin@gmail.com>
2020-08-31 15:25:45 -0400

    updated language and words to german

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,20 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'German', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'ein', 'one', 2),
+  (2, 1, 'zwei', 'two', 3),
+  (3, 1, 'drei', 'three', 4),
+  (4, 1, 'vier', 'four', 5),
+  (5, 1, 'fünf', 'five', 6),
+  (6, 1, 'sechs', 'six', 7),
+  (7, 1, 'sieben', 'seven', 8),
+  (8, 1, 'acht', 'eight', 9),
+  (9, 1, 'nine', 'neun', 10),
+  (10, 1, 'zehn', 'ten', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
