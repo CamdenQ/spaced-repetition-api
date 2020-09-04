f9dca94364d0dd3d2087c07d0b760aa70b527cb6
Camden Miller <morrivar@outlook.com>
2020-09-04 10:36:27 -0400

    refactored language table queries to use user_id

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -6,97 +6,98 @@ const LinkedList = require('./linked-list');
 const languageRouter = express.Router();
 
 languageRouter.use(requireAuth).use(async (req, res, next) => {
-	try {
-		const language = await LanguageService.getUsersLanguage(
-			req.app.get('db'),
-			req.user.id
-		);
+  try {
+    const language = await LanguageService.getUsersLanguage(
+      req.app.get('db'),
+      req.user.id
+    );
 
-		if (!language)
-			return res.status(404).json({
-				error: `You don't have any languages`,
-			});
+    if (!language)
+      return res.status(404).json({
+        error: `You don't have any languages`,
+      });
 
-		req.language = language;
-		next();
-	} catch (error) {
-		next(error);
-	}
+    req.language = language;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 languageRouter.get('/', async (req, res, next) => {
-	try {
-		const words = await LanguageService.getLanguageWords(
-			req.app.get('db'),
-			req.language.id
-		);
+  try {
+    const words = await LanguageService.getLanguageWords(
+      req.app.get('db'),
+      req.language.id
+    );
 
-		res.json({
-			language: req.language,
-			words,
-		});
-		next();
-	} catch (error) {
-		next(error);
-	}
+    res.json({
+      language: req.language,
+      words,
+    });
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 languageRouter.get('/head', async (req, res, next) => {
-	try {
-		const [nextWord] = await LanguageService.getNextWord(
-			req.app.get('db'),
-			req.language.id
-		);
-		res.json({
-			nextWord: nextWord.original,
-			totalScore: req.language.total_score,
-			correctCount: nextWord.correct_count,
-			incorrectCount: nextWord.incorrect_count,
-		});
-		next();
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const [nextWord] = await LanguageService.getNextWord(
+      req.app.get('db'),
+      req.language.id
+    );
+    res.json({
+      nextWord: nextWord.original,
+      totalScore: req.language.total_score,
+      correctCount: nextWord.correct_count,
+      incorrectCount: nextWord.incorrect_count,
+    });
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 languageRouter.route('/guess').post(express.json(), async (req, res, next) => {
-	try {
-		const guess = req.body.guess;
-		const db = req.app.get('db');
-		const languageId = req.language.id;
+  try {
+    const guess = req.body.guess;
+    const db = req.app.get('db');
+    const languageId = req.language.id;
+    const userId = req.user.id;
 
-		let headId = await LanguageService.getHeadId(db);
+    let headId = await LanguageService.getHeadId(db, userId);
 
-		headId = headId[0].head;
+    headId = headId[0].head;
 
-		let head = await LanguageService.getWordById(db, headId);
+    let head = await LanguageService.getWordById(db, headId);
 
-		let words = await LanguageService.getLanguageWords(db, languageId);
+    let words = await LanguageService.getLanguageWords(db, languageId);
 
-		let wordsList = LanguageService.createList(head, words);
+    let wordsList = LanguageService.createList(head, words);
 
-		let score = await LanguageService.getScore(db);
-		score = score[0].total_score;
+    let score = await LanguageService.getScore(db);
+    score = score[0].total_score;
 
-		const isCorrect = LanguageService.checkGuess(guess, wordsList);
+    const isCorrect = LanguageService.checkGuess(guess, wordsList);
 
-		if (isCorrect === true) {
-			wordsList.head.value.memory_value = wordsList.head.value.memory_value * 2;
-			await LanguageService.updateScore(db, score);
-		}
+    if (isCorrect === true) {
+      wordsList.head.value.memory_value = wordsList.head.value.memory_value * 2;
+      await LanguageService.updateScore(db, score, userId);
+    }
 
-		let currentWord = wordsList.remove(wordsList.head);
+    let currentWord = wordsList.remove(wordsList.head);
 
-		await LanguageService.updateMemory(db, currentWord);
+    await LanguageService.updateMemory(db, currentWord);
 
-		wordsList.insertAt(currentWord.value.memory_value, currentWord.value);
+    wordsList.insertAt(currentWord.value.memory_value, currentWord.value);
 
-		await LanguageService.insertList(db, wordsList);
-		await LanguageService.updateHead(db, wordsList);
-		res.send(wordsList);
-	} catch (error) {
-		next(error);
-	}
+    await LanguageService.insertList(db, wordsList);
+    await LanguageService.updateHead(db, wordsList, userId);
+    res.send(wordsList);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = languageRouter;
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,124 +1,130 @@
 const LinkedList = require('./linked-list');
 
 const LanguageService = {
-	getUsersLanguage(db, user_id) {
-		return db
-			.from('language')
-			.select(
-				'language.id',
-				'language.name',
-				'language.user_id',
-				'language.head',
-				'language.total_score'
-			)
-			.where('language.user_id', user_id)
-			.first();
-	},
-	getWordById(db, id) {
-		return db.from('word').select('*').where({ id: id });
-	},
+  getUsersLanguage(db, user_id) {
+    return db
+      .from('language')
+      .select(
+        'language.id',
+        'language.name',
+        'language.user_id',
+        'language.head',
+        'language.total_score'
+      )
+      .where('language.user_id', user_id)
+      .first();
+  },
+  getWordById(db, id) {
+    return db.from('word').select('*').where({ id: id });
+  },
 
-	getHeadId(db) {
-		return db.from('language').select('language.head');
-	},
+  getHeadId(db, user_id) {
+    return db
+      .from('language')
+      .select('language.head')
+      .where('language.user_id', user_id);
+  },
 
-	getScore(db) {
-		return db.from('language').select('language.total_score');
-	},
+  getScore(db, user_id) {
+    return db
+      .from('language')
+      .select('language.total_score')
+      .where('language.user_id', user_id);
+  },
 
-	getLanguageWords(db, language_id) {
-		return db
-			.from('word')
-			.select(
-				'id',
-				'language_id',
-				'original',
-				'translation',
-				'next',
-				'memory_value',
-				'correct_count',
-				'incorrect_count'
-			)
-			.where({ language_id });
-	},
+  getLanguageWords(db, language_id) {
+    return db
+      .from('word')
+      .select(
+        'id',
+        'language_id',
+        'original',
+        'translation',
+        'next',
+        'memory_value',
+        'correct_count',
+        'incorrect_count'
+      )
+      .where({ language_id });
+  },
 
-	getNextWord(db, language_id) {
-		return db
-			.from('word')
-			.join('language', 'word.id', '=', 'language.head')
-			.select('original', 'language_id', 'correct_count', 'incorrect_count')
-			.where({ language_id });
-	},
+  getNextWord(db, language_id) {
+    return db
+      .from('word')
+      .join('language', 'word.id', '=', 'language.head')
+      .select('original', 'language_id', 'correct_count', 'incorrect_count')
+      .where({ language_id });
+  },
 
-	checkGuess(guess, words) {
-		let isCorrect = guess.trim().toLowerCase() === words.head.value.translation;
+  checkGuess(guess, words) {
+    let isCorrect = guess.trim().toLowerCase() === words.head.value.translation;
 
-		return isCorrect;
-	},
+    return isCorrect;
+  },
 
-	insertList(db, list) {
-		let counter = 0;
-		let currNode = list.head;
+  insertList(db, list) {
+    let counter = 0;
+    let currNode = list.head;
 
-		while (counter < list.length - 1) {
-			db.update({ next: currNode.next.value.id })
-				.from('word')
-				.where({ id: currNode.value.id })
-				.then(() => (counter = counter));
-			currNode = currNode.next;
-			counter++;
-		}
-		return db
-			.update({ next: null })
-			.from('word')
-			.where({ id: currNode.value.id });
-	},
+    while (counter < list.length - 1) {
+      db.update({ next: currNode.next.value.id })
+        .from('word')
+        .where({ id: currNode.value.id })
+        .then(() => (counter = counter));
+      currNode = currNode.next;
+      counter++;
+    }
+    return db
+      .update({ next: null })
+      .from('word')
+      .where({ id: currNode.value.id });
+  },
 
-	updateMemory(db, head) {
-		// prettier-ignore
-		return db.from('word')
+  updateMemory(db, head) {
+    // prettier-ignore
+    return db.from('word')
 			.where({ id: head.value.id }).update({ memory_value: head.value.memory_value });
-	},
-	updateHead(db, list) {
-		return db
-			.from('language')
-			.where({ id: list.head.value.language_id })
-			.update({ head: list.head.value.id });
-	},
-	updateScore(db, score) {
-		let newScore = score + 1;
-		return db
-			.from('language')
-			.update({ total_score: newScore })
-			.where({ id: 1 });
-	},
-	increaseCorrectCount(db, word) {
-		let newCount = word.value.correctCount + 1;
-		return db
-			.from('word')
-			.where({ id: word.value.id })
-			.update({ correct_count: newCount });
-	},
-	increaseIncorrectCount(db, word) {
-		let newCount = word.value.incorrectCount + 1;
-		return db
-			.from('word')
-			.where({ id: word.value.id })
-			.update({ incorrect_count: newCount });
-	},
-	createList(head, arr) {
-		let currNode = head[0];
-		let list = new LinkedList();
-		list.insertLast(currNode);
+  },
+  updateHead(db, list, user_id) {
+    return db
+      .from('language')
+      .where('language.user_id', user_id)
+      .update({ head: list.head.value.id });
+  },
+  updateScore(db, score, user_id) {
+    let newScore = score + 1;
+    return db
+      .from('language')
+      .update({ total_score: newScore })
+      .where('language.user_id', user_id);
+  },
+  increaseCorrectCount(db, word) {
+    let newCount = word.value.correctCount + 1;
+    return db
+      .from('word')
+      .where({ id: word.value.id })
+      .update({ correct_count: newCount });
+  },
+  increaseIncorrectCount(db, word) {
+    let newCount = word.value.incorrectCount + 1;
+    return db
+      .from('word')
+      .where({ id: word.value.id })
+      .update({ incorrect_count: newCount });
+  },
+  createList(head, arr) {
+    let currNode = head[0];
+    let list = new LinkedList();
+    list.insertLast(currNode);
 
-		while (currNode.next !== null) {
-			let wordToInsert = arr.find((word) => word.id === currNode.next);
-			list.insertLast(wordToInsert);
+    while (currNode.next !== null) {
+      let wordToInsert = arr.find((word) => word.id === currNode.next);
+      list.insertLast(wordToInsert);
 
-			currNode = wordToInsert;
-		}
-		return list;
-	},
+      currNode = wordToInsert;
+    }
+    return list;
+  },
 };
 
 module.exports = LanguageService;
